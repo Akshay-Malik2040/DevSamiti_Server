375284faa8206881ee69024527839ffa17c12fd0
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2025-11-22 16:18:34 +0530

    Update sendRequest logic to not send request to same user

--- src/controllers/requestController.js
+++ src/controllers/requestController.js
@@ -1,20 +1,44 @@
-const Connection=require('../models/Connection')
+const Connection = require('../models/Connection')
+const User = require('../models/User')
 
-const sendRequest=async(req,res)=>{
-    try{
-        const fromUserId=req.user._id;
-        const toUserId=req.params.toUserId;
-        const status=req.params.status;
+const sendRequest = async (req, res) => {
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
 
-        const allowedStatus=['ignored','interested'];
-        if(!allowedStatus.includes(status)) throw new Error("Invalid Status");
+        const allowedStatus = ['ignored', 'interested'];
+        if (!allowedStatus.includes(status)) throw new Error("Invalid Status");
 
-        const connection=await Connection.create({toUserId,fromUserId,status});
-        res.json({message:"Request Sent",connection});
+        const existingConnectionRequest = await Connection.findOne({
+            $or: [
+                {
+                    fromUserId, toUserId
+                }, {
+                    fromUserId: toUserId, toUserId: fromUserId
+                }
+            ]
+        })
 
-    } catch(err){
-        res.status(400).json({Error:err.message})
+        if (existingConnectionRequest) throw new Error("Connection Request already exists")
+
+        const toUser = await User.findById(toUserId).exec();
+        if (!toUser) throw new Error("User not found")
+
+        if (toUserId.toString() === fromUserId.toString()) {
+            throw new Error("Cannot send req to Yourself")
+        }
+
+        const connection = await Connection.create({ toUserId, fromUserId, status });
+        res.json({ message: "Request Sent", connection });
+
+    } catch (err) {
+        res.status(400).json({ Error: err.message })
     }
 }
 
-module.exports={sendRequest}
+const reviewRequest = async (req, res) => {
+
+}
+
+module.exports = { sendRequest }
