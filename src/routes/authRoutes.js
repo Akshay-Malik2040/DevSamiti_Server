42d55b0258d564d201d062d0de87835cc41b2656
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2025-11-22 12:48:22 +0530

    Add Logout authRouter

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,20 @@
-const express=require('express');
-const authRouter=express.Router();
-const {signup,login}=require('../controllers/authController')
+const express = require('express');
+const authRouter = express.Router();
+const { signup, login } = require('../controllers/authController')
 
-authRouter.post('/signup',signup);
+authRouter.post('/signup', signup);
 
-authRouter.post('/login',login);
+authRouter.post('/login', login);
 
-module.exports=authRouter;
+authRouter.post('/logout', (req, res) => {
+    try {
+        const { token } = req.cookies;
+        if (!token) throw new Error("Login first")
+        res.cookie("token",null,{expires:new Date(Date.now())});
+        res.json({message:"Logout Successfully"})
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
+
+module.exports = authRouter;
